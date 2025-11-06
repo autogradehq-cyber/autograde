c9aed4bbc795a4a658916187c486493cc49719e0
Andre Meyers <falconqualitybuilders@macbookpro.mynetworksettings.com>
2025-11-06 15:33:07 -0500

    fix: restore default export in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,46 @@
+// src/pages/_app.tsx
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-// ...inside the component render:
-{GA_ID && (
-  <>
-    <Script
-      id="ga4-src"
-      strategy="afterInteractive"
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-    />
-    <Script id="ga4-init" strategy="afterInteractive">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${GA_ID}', { send_page_view: true });
-        window.gtag = gtag;
-      `}
-    </Script>
-  </>
-)}
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!GA_ID) return;
+    const handleRouteChange = (url: string) => {
+      // @ts-expect-error gtag is defined by GA script
+      window.gtag?.("event", "page_view", { page_location: url });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
+  return (
+    <>
+      {GA_ID && (
+        <>
+          <Script
+            id="ga4-src"
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          />
+          <Script id="ga4-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', { send_page_view: true });
+              window.gtag = gtag;
+            `}
+          </Script>
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
+}
